5ed5cd376df45cfa7257249c4fa4b357e0f8f4ea
gabrielseventhucsd25 <103284140+gabrielseventhucsd25@users.noreply.github.com>
2022-06-03 12:22:40 -0700

    added commonmark

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,12 +11,47 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    /*
     @Test
     public void testBreak1() throws IOException{
         ArrayList<String> a = new ArrayList<>();
-        Path p = Path.of("break-test.md");
+        Path p = Path.of("/Users/hihowareu/Documents/GitHub/markdown-parser/break-test.md");
         String content = Files.readString(p);
         a.add("https://en.wikipedia.org/wiki/Diego_(tortoise)");
+        assertEquals(a, MarkdownParse.getLinks(content));
+    }
+    */
+    @Test
+    public void testSnippet1() throws IOException{
+        ArrayList<String> a = new ArrayList<>();
+        Path p = Path.of("/Users/hihowareu/Documents/GitHub/markdown-parser/snippet1.md");
+        String content = Files.readString(p);
+        a.add("url.com");
+        a.add("google.com");
+        a.add("google.com");
+        a.add("ucsd.edu");
+        assertEquals(a, MarkdownParse.getLinks(content));
+    }
+    @Test
+    public void testSnippet2() throws IOException{
+        ArrayList<String> a = new ArrayList<>();
+        Path p = Path.of("/Users/hihowareu/Documents/GitHub/markdown-parser/snippet2.md");
+        String content = Files.readString(p);
+        a.add("a.com");
+        a.add("b.com");
+        a.add("a.com");
+        a.add("example.com");
+        assertEquals(a, MarkdownParse.getLinks(content));
+    }
+    @Test
+    public void testSnippet3() throws IOException{
+        ArrayList<String> a = new ArrayList<>();
+        Path p = Path.of("/Users/hihowareu/Documents/GitHub/markdown-parser/snippet3.md");
+        String content = Files.readString(p);
+        a.add("https://www.twitter.com");
+        a.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        a.add("github.com");
+        a.add("https://cse.ucsd.edu/");
         assertEquals(a, MarkdownParse.getLinks(content));
     }
 }
